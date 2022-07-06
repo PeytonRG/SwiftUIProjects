07e7b0dec99a82b550d4aa5087c6a490729a1cc9
Peyton Gasink <peyton@peytongasink.dev>
2022-07-05 20:21:12 -0500

    created app

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Peyton Gasink on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
