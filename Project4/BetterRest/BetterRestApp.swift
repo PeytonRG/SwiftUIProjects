8b0161f40106797b1acf47e504c0f94dfdcd8d4f
Peyton Gasink <peyton@peytongasink.dev>
2022-07-31 20:59:20 -0500

    created project 4 and ML model

---
+++ Project4/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Peyton Gasink on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project4/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Peyton Gasink on 7/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
