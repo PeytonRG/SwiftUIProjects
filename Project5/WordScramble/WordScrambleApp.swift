22fb15bbd8d2b616b929c279015fee89050ce489
Peyton Gasink <peyton@peytongasink.dev>
2022-08-03 19:13:29 -0500

    created project 5

---
+++ Project5/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Peyton Gasink on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
