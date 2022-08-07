fca254e60d9d02ed943349a99be907824f0fa2d8
Peyton Gasink <peyton@peytongasink.dev>
2022-08-06 20:35:56 -0500

    project 6 day 1

---
+++ Project6/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Peyton Gasink on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
