6ed8baa06d79509416f73f8e0b909e9829a8e03b
Peyton Gasink <peyton@peytongasink.dev>
2022-08-24 21:25:15 -0500

    finished but with a bug
    and I'm not gonna fix it, but I think I know how I would

--- Project7/iExpense/ContentView.swift
+++ Project7/iExpense/ContentView.swift
@@ -14,21 +14,51 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
-                        }
-                        
-                        Spacer()
-                        
-                        Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                            .foregroundColor(item.amount < 100 ? (item.amount < 10 ? .green : .blue) : .red)
+                Section {
+                    let personalExpenses = expenses.items.filter { item in
+                        return item.type == "Personal"
                     }
+                    ForEach(personalExpenses) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                .foregroundColor(item.amount < 100 ? (item.amount < 10 ? .green : .blue) : .red)
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                } header: {
+                    Text("Personal Expenses")
                 }
-                .onDelete(perform: removeItems)
+                
+                Section {
+                    let businessExpenses = expenses.items.filter { item in
+                        return item.type == "Business"
+                    }
+                    ForEach(businessExpenses) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                .foregroundColor(item.amount < 100 ? (item.amount < 10 ? .green : .blue) : .red)
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                } header: {
+                    Text("Business Expenses")
+                }
             }
             .navigationTitle("iExpense")
             .toolbar {
@@ -45,6 +75,7 @@ struct ContentView: View {
     }
     
     func removeItems(at offsets: IndexSet) {
+        // idk how to remove from the correct list without just splitting the lists in two and I don't wanna do that for just this challenge so this bug will remain
         expenses.items.remove(atOffsets: offsets)
     }
 }
