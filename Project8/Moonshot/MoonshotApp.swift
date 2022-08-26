98947824d178a13249010ff1853872ac130e779c
Peyton Gasink <peyton@peytongasink.dev>
2022-08-25 21:32:21 -0500

    project 8 day 1

---
+++ Project8/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Peyton Gasink on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
