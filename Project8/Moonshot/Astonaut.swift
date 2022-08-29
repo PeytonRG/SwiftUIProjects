9260a568f9bbb83d0b12b0ede1e831fa3ff833c3
Peyton Gasink <peyton@peytongasink.dev>
2022-08-28 21:31:51 -0500

    project 8 day 2

---
+++ Project8/Moonshot/Astonaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astonaut.swift
+//  Moonshot
+//
+//  Created by Peyton Gasink on 8/28/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
