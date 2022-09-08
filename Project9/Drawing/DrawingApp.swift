4cd30944835a1a0548ad838dd3333becc3124b2d
Peyton Gasink <peyton@peytongasink.dev>
2022-09-07 21:01:00 -0500

    project 9 day 1

---
+++ Project9/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Peyton Gasink on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
