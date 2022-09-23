c833163919adeeea0b1d7d036ba0c954edbda3ea
Peyton Gasink <peyton@peytongasink.dev>
2022-09-22 21:20:46 -0500

    project 10 day 1

---
+++ Project10/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Peyton Gasink on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
