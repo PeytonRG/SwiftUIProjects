10a4bfa0d491a7f903586b827ed6b5c6e083ec8e
Peyton Gasink <peyton@peytongasink.dev>
2022-10-06 21:11:46 -0500

    project 12 day 1

---
+++ Project12/CoreDataProject/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Peyton Gasink on 10/6/22.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
