648b48d4f5741cffee014f0f2944956d2b6f3d2b
Peyton Gasink <peyton@peytongasink.dev>
2022-10-07 21:38:32 -0500

    project 12 day 2

---
+++ Project12/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Peyton Gasink on 10/7/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
