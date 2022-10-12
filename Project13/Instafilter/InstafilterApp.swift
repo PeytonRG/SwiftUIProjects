9f16bb5fe3cadc2ffbf1231f5f3df6c918fbc899
Peyton Gasink <peyton@peytongasink.dev>
2022-10-11 20:25:51 -0500

    project 13 day 1

---
+++ Project13/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Peyton Gasink on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
