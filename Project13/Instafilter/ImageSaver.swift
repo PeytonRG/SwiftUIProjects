b444c02d1d375c992be10006a517d11eb1f82fb9
Peyton Gasink <peyton@peytongasink.dev>
2022-10-27 19:40:32 -0500

    project 13 day 3

---
+++ Project13/Instafilter/ImageSaver.swift
@@ -0,0 +1,19 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Peyton Gasink on 10/27/22.
+//
+
+import Foundation
+import UIKit
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# WeSplit
+# 100 Days of SwiftUI
 
 Projects for [100 Days of SwiftUI](https://www.hackingwithswift.com/100/swiftui).
