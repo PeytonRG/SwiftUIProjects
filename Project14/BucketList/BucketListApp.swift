0176b3248e40ef2e58c87f847cc1254eedcd8962
Peyton Gasink <peyton@peytongasink.dev>
2022-11-01 20:24:26 -0500

    project 14 day 1

---
+++ Project14/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Peyton Gasink on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
