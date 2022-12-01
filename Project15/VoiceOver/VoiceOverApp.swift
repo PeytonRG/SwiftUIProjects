576cfc90bdf6f54d0b13c83810b1fbe7010d77cd
Peyton Gasink <peyton@peytongasink.dev>
2022-12-01 17:39:05 -0600

    project 15 day 1

---
+++ Project15/VoiceOver/VoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOverApp.swift
+//  VoiceOver
+//
+//  Created by Peyton Gasink on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
