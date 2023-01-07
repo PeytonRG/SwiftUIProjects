90a6e294b26034f110f84ef1cd6020f1eddc5943
Peyton Gasink <peyton@peytongasink.dev>
2023-01-06 19:18:12 -0600

    project 15 day 2
    it's been a minute

--- Project2/GuessTheFlag/ContentView.swift
+++ Project2/GuessTheFlag/ContentView.swift
@@ -8,6 +8,20 @@
 import SwiftUI
 
 struct FlagImage: View {
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     var country: String
     
     var body: some View {
@@ -15,6 +29,7 @@ struct FlagImage: View {
             .renderingMode(.original)
             .clipShape(Capsule())
             .shadow(radius: 5)
+            .accessibilityLabel(labels[country, default: "Unknown flag"])
     }
 }
 
@@ -24,6 +39,7 @@ struct ContentView: View {
     @State private var score = 0
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    
     @State private var alertText = ""
     @State private var questionNumber = 1
     @State private var continueButtonText = ""
